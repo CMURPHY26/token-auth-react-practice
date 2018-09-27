62b00a7ea5188c4e114328a7ef67423fd40e1a9f
Bob Ziroll <elderziroll@gmail.com>
2018-09-27 15:00:43 -0600

    Updated auth routes to include new error handling methods

--- routes/auth.js
+++ routes/auth.js
@@ -3,26 +3,35 @@ const User = require("../models/user");
 const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
 
-authRouter.post("/signup", (req, res) => {
+authRouter.post("/signup", (req, res, next) => {
     User.findOne({username: req.body.username}, (err, existingUser) => {
-        if (err) return res.status(500).send({success: false, err});
-        if (existingUser !== null) {
-            return res.status(400).send({success: false, err: "That username already exists!"});
+        if (err) {
+            res.status(500);
+            return next(err);
+        } else if (existingUser !== null) {
+            res.status(400);
+            return next(new Error("That username already exists!"));
         }
         const newUser = new User(req.body);
         newUser.save((err, user) => {
-            if (err) return res.status(500).send({success: false, err});
+            if (err) {
+                res.status(500);
+                return next(err);
+            }
             const token = jwt.sign(user.toObject(), process.env.SECRET);
             return res.status(201).send({success: true, user: user.toObject(), token});
         });
     });
 });
 
-authRouter.post("/login", (req, res) => {
+authRouter.post("/login", (req, res, next) => {
     User.findOne({username: req.body.username.toLowerCase()}, (err, user) => {
-        if (err) return res.status(500).send(err);
-        if (!user || user.password !== req.body.password) {
-            return res.status(403).send({success: false, err: "Username or password are incorrect"})
+        if (err) {
+            res.status(500);
+            return next(err);
+        } else if (!user || user.password !== req.body.password) {
+            res.status(403);
+            return next(new Error("Username or password are incorrect"));
         }
         const token = jwt.sign(user.toObject(), process.env.SECRET);
         return res.send({token: token, user: user.toObject(), success: true})
